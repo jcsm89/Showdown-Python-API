9d21981c8361ed7e3e7d399515e6c9c3ea004a79
joao.monteiro <joao.monteiro@vision-box.com>
2024-10-24 13:46:13 +0100

    First draft of pokemon class

--- data_model/interface.py
+++ data_model/interface.py
@@ -1,29 +1,32 @@
 from enum import Enum
 
 
+class DataType(Enum):
+    """
+    Enum of all supported data types.
+    """
+    Name = 0    # TODO: This is not in the original Enum, investigate further
+    Ability = 1
+    Item = 2
+    Move = 3
+    Species = 4
+
+
 class Data:
     """
     Base data class, from which some others inherit.
     """
+    # TODO: Check how this two are filled
+    id: str
+    name: str
+    type: DataType
 
-    def __init__(self) -> None:
+    def __init__(self,
+                 type: DataType) -> None:
         """
         Class constructor.
         """
-        pass
-
-
-class Generation:
-    """
-    Class with all relevant info of a generation.
-    """
-    pass
-
-    def __init__(self) -> None:
-        """
-        Class constructor.
-        """
-        pass
+        self.type = type
 
 
 class Name(Data):
@@ -35,13 +38,65 @@ class Name(Data):
         """
         Class constructor.
         """
-        super.__init__()
+        super.__init__(type=DataType.Name)
         pass
 
 
-class Species:
+class Ability(Data):
+    """
+    Class storing an ability name and ID.
+    """
+
+    def __init__(self) -> None:
+        """
+        Class constructor.
+        """
+        super.__init__(type=DataType.Ability)
+        pass
+
+
+class Item(Data):
+    """
+    Class storing an item name and ID.
+    """
+
+    def __init__(self) -> None:
+        """
+        Class constructor.
+        """
+        super.__init__(type=DataType.Item)
+        pass
+
+
+class Move(Data):
+    """
+    Class with all relevant info for all a Pokemon move.
+    """
+
+    def __init__(self) -> None:
+        """
+        Class constructor.
+        """
+        super.__init__(type=DataType.Move)
+        pass
+
+
+class Species(Data):
     """
     Class with all relevant info for all variation of a Pokemon given its name.
+    """
+
+    def __init__(self) -> None:
+        """
+        Class constructor.
+        """
+        super.__init__(type=DataType.Species)
+        pass
+
+
+class Generation:
+    """
+    Class with all relevant info of a generation.
     """
 
     def __init__(self) -> None:
@@ -51,15 +106,60 @@ class Species:
         pass
 
 
-class TypeName(Enum):
+class Moveset:
+    """
+    Class with all relevant information of a Pokemon's moveset.
+    """
+
+    move_1: Move
+    move_2: Move
+    move_3: Move
+    move_4: Move
+
+    def __init__(self,
+                 move_1: Move,
+                 move_2: Move,
+                 move_3: Move,
+                 move_4: Move) -> None:
+        """
+        Class constructor.
+        """
+        self.move_1 = move_1
+        self.move_2 = move_2
+        self.move_3 = move_3
+        self.move_4 = move_4
+
+
+class Type(Enum):
     """
     Enum of all possible Pokemon types.
     """
     pass
 
 
-class GenderName(Enum):
+class Gender(Enum):
     """
     Enum of all possible Pokemon genders.
+    """
+    pass
+
+
+class Stat(Enum):
+    """
+    Enum of all possible Pokemon stats.
+    """
+    pass
+
+
+class Nature(Enum):
+    """
+    Enum of all possible Pokemon natures.
+    """
+    pass
+
+
+class Status(Enum):
+    """
+    Enum of all possible Pokemon status conditions.
     """
     pass
--- data_model/pokemon.py
+++ data_model/pokemon.py
@@ -2,42 +2,45 @@ from typing import Union, Optional
 
 import interface
 
+
 class Pokemon:
     """
     Base class with all relevant information to describe a Pokemon.
     """
 
+    generation: interface.Generation
+    name: interface.Name
+    species: interface.Species
+    types: Union[interface.Type, tuple[interface.Type, interface.Type]]
+    weight_kg: int
+    level: int
+
+    gender: Optional[interface.Gender]
+    ability: Optional[interface.Ability]
+    ability_on: Optional[bool]
+    is_dynamaxed: Optional[bool]
+    dynamax_level: Optional[int]
+    is_salt_cure: Optional[bool]
+    allies_fainted: Optional[int]
+    boosted_stat: Optional[interface.Stat]
+    item: Optional[str]
+    teraType: Optional[str]
+
+    nature: interface.Nature
+    ivs: dict[interface.Stat, int]
+    evs: dict[interface.Stat, int]
+    boosts: dict[interface.Stat, float]
+    raw_stats: dict[interface.Stat, int]
+    stats: dict[interface.Stat, int]
+
+    original_curr_hp: int
+    status: Optional[interface.Status]
+    toxic_counter: int
+
+    moves: interface.Moveset
+
     def __init__(self) -> None:
         """
         Class constructor.
         """
-        generation: interface.Generation
-        name: interface.Name
-        species: interface.Species
-        types: Union[interface.TypeName, tuple[interface.TypeName]]
-        weight_kg: int
-        level: int
-
-        gender: Optional[interface.GenderName]
-        ability: Optional[str]
-        abilityOn: Optional[bool]
-        is_dynamaxed: Optional[bool]
-        dynamax_level: Optional[int]
-        is_salt_cure: Optional[bool]
-        allies_fainted: Optional[int]
-        boostedStat?: I.StatIDExceptHP | 'auto';
-        item?: I.ItemName;
-        teraType?: I.TypeName;
-
-        nature: I.NatureName;
-        ivs: I.StatsTable;
-        evs: I.StatsTable;
-        boosts: I.StatsTable;
-        rawStats: I.StatsTable;
-        stats: I.StatsTable;
-
-        originalCurHP: number;
-        status: I.StatusName | '';
-        toxicCounter: number;
-
-        moves: I.MoveName[];
+        pass
